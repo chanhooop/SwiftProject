76a5459450c85ddf69d4692d9323241c49bb3e30
chanhooop <cksghwwkd@gmail.com>
2021-07-29 15:54:34 +0900

    dsfgfsg

--- BabyProject/BabyProject/Controller/MainTableViewController.swift
+++ BabyProject/BabyProject/Controller/MainTableViewController.swift
@@ -14,7 +14,7 @@ class MainTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//tftvct
+//tftvctadad
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
